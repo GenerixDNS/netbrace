d61e9d0cdd7e7ff419f73443d01fca50f264d4cf
Generix <Lightmicrash@gmail.com>
2022-07-04 00:24:39 +0200

    native commit

---
+++ netbrace-core/target/debug/deps/netbrace-b3ff85ec39c6c554.d
@@ -0,0 +1,5 @@
+C:\Users\Alexa\Documents\programming\projects\netbrace\netbrace-core\target\debug\deps\netbrace-b3ff85ec39c6c554.exe: src\main.rs
+
+C:\Users\Alexa\Documents\programming\projects\netbrace\netbrace-core\target\debug\deps\netbrace-b3ff85ec39c6c554.d: src\main.rs
+
+src\main.rs:
---
+++ netbrace-core/target/debug/netbrace.d
@@ -0,0 +1,1 @@
+C:\Users\Alexa\Documents\programming\projects\netbrace\netbrace-core\target\debug\netbrace.exe: C:\Users\Alexa\Documents\programming\projects\netbrace\netbrace-core\src\main.rs
