d13101ba8d3871975892cb728fcb3add9ca6b67f
Generix <Lightmicrash@gmail.com>
2022-07-04 00:30:42 +0200

    api commit - api, common

---
+++ netbrace-api/src/main/java/org/netbrace/api/IBootstrap.java
@@ -0,0 +1,14 @@
+package org.netbrace.api;
+
+/**
+ * this doc was created on 04.07.2022
+ * This class belongs to the netbrace project
+ *
+ * @author Generix030
+ */
+
+public interface IBootstrap {
+
+
+
+}
---
+++ netbrace-common/src/main/java/org/netbrace/common/BootstrapImpl.java
@@ -0,0 +1,16 @@
+package org.netbrace.common;
+
+import org.netbrace.api.IBootstrap;
+
+/**
+ * this doc was created on 04.07.2022
+ * This class belongs to the netbrace project
+ *
+ * @author Generix030
+ */
+
+public class BootstrapImpl implements IBootstrap {
+
+
+
+}
